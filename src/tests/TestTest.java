37184f221c3f3020c75ee5fb174fe6a1ebf1740c
Oleki <olekignaczak78@gmail.com>
2024-10-31 13:41:23 +0100

    Spritesheet

--- src/tests/TestTest.java
+++ src/tests/TestTest.java
@@ -22,57 +22,57 @@ public class TestTest
     @Test
     public void testAdd()
     {
-        Assertions.assertEquals(5, myMath.add(2, 3));
+        Assertions.assertEquals(5, myMath.add(2, 3), "[Adding 2 digits]");
     }
 
     @Test
     public void testSubtract()
     {
-        Assertions.assertEquals(2, myMath.subtract(3, 2), "XDDDDDD", "XDDASD");
+        Assertions.assertEquals(2, myMath.subtract(3, 2), "[Subtracting 2 digits]");
     }
 
     @Test
     public void testAssertFalse()
     {
-        Assertions.assertFalse(myMath.add(1, 1) == 2);
+        Assertions.assertFalse(myMath.add(1, 1) == 2, "[Comparing, if 1+1 = 2 isn't two");
     }
 
     @Test
     public void testAssertTrue()
     {
-        Assertions.assertTrue(myMath.add(1, 1) == 3);
+        Assertions.assertTrue(myMath.add(1, 1) == 3, "[Comparing, if 1+1 = 3 is true");
     }
 
     @Test
     public void testAssertNotNull()
     {
-        Assertions.assertNotNull(myMath);
+        Assertions.assertNotNull(myMath, "[Checking if myMath object exists]");
     }
 
     @Test
     public void testAssertNull()
     {
         tearDown();
-        Assertions.assertNull(myMath);
+        Assertions.assertNull(myMath, "[Checking if myMath object does not exist]");
     }
 
     @Test
     public void testAssertNotEquals()
     {
-        Assertions.assertNotEquals(3, myMath.add(2, 2));
+        Assertions.assertNotEquals(3, myMath.add(2, 2), "[Checking, if 2+2 equals 3]");
     }
 
     @Test
     public void testAssertSame()
     {
         MyMath instance = myMath;
-        Assertions.assertSame(myMath, instance);
+        Assertions.assertSame(myMath, instance, "[Checking if two instances of the same class are same]");
     }
 
     @Test
     public void testAssertNotSame()
     {
         MyMath instance = new MyMath();
-        Assertions.assertNotSame(myMath, instance);
+        Assertions.assertNotSame(myMath, instance, "[Checking if two instances of the same class are not same]");
     }
 }
