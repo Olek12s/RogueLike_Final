1728fd0cf28b8b52d0fd606447f33222d3b67df3
Oleki <olekignaczak78@gmail.com>
2024-11-23 01:01:59 +0100

    health bar added

--- src/ui/HUDRenderer.java
+++ src/ui/HUDRenderer.java
@@ -2,6 +2,7 @@ package ui;
 
 import main.DrawPriorities;
 import main.Drawable;
+import org.w3c.dom.css.RGBColor;
 
 import java.awt.*;
 
@@ -24,6 +25,18 @@ public class HUDRenderer implements Drawable
     @Override
     public void draw(Graphics g2)
     {
-        g2.drawImage(hud.heart.image, 0, 0, hud.scale, hud.scale, null);
+        Graphics2D g2d = (Graphics2D) g2;
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(3));
+
+        if (hud.gc.player.hitPoints < 0) hud.gc.player.hitPoints = 0;
+        int healthPercent = (int) ((hud.gc.player.hitPoints / (double) hud.gc.player.getMaxHitPoints()) * 100);
+
+        g2d.drawRect(45, 24, 300, 26);
+        g2d.setColor(new Color(75, 61, 61, 210));
+        g2d.fillRect(46, 25, 299, 25);
+        g2d.setColor(Color.RED);
+        g2d.fillRect(46, 25, (healthPercent*299)/100, 25);
+        g2d.drawImage(hud.heart.image, 0, 0, hud.scale, hud.scale, null);
     }
 }
